c16caafff0a246b5ca031f9b88747cc5337991c0
mofasa4 <moh.f.saleh4@gmail.com>
2020-10-02 04:48:32 +0200

    deleted the about page and controller and created a todos controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    To Do List
+                    <a href="/todos">To Do List</a>
                 </div>
             </div>
         </div>
