243b113d1a86758c6db944b67b24c2388543d558
mofasa4 <moh.f.saleh4@gmail.com>
2020-10-06 15:50:02 +0200

    creating the layouts file

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" crossorigin="anonymous">
-    <title>Todos</title>
-</head>
-<body>
-    <div class="container">
+@extends('layouts.app')
+
+@section('title')
+To Do List
+@endsection
+
+
+@section('content')
         <h2 class="text-center my-5">Todos Page</h2>
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -28,6 +26,4 @@
                 </div>
             </div>
         </div>
-    </div>
-</body>
-</html>
+@endsection
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" crossorigin="anonymous">
-    <title>Show To Do</title>
-</head>
-<body>
-<div class="container">
+@extends('layouts.app')
+
+@section('title')
+Show Item
+@endsection
+
+
+@section('content')
     <h3 class="text-center my-5">{{$toddo->name}}</h3>
     <div class="row justify-content-center">
         <div class="col-md-6">
@@ -19,7 +17,7 @@
         </div>
         </div>
     </div>
-</div>
+
+
+@endsection
     
-</body>
-</html>
